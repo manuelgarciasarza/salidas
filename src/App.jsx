1152cded4339e3bf2c58fdacb39e99b9d8a34f1c
manuel garcia sarza <manuelgarciasarza@gmail.com>
2023-11-15 18:04:12 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+
+// components
+import Home from "./views/home/home.jsx";
+
+function MainApp() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />{" "}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default MainApp;
